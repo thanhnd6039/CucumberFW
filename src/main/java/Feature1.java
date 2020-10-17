80a6b3b32205f27efa8d1f7fb29e1ae1205687e7
thanh.nguyen <thanhvirtium@gmail.com>
2020-10-17 22:26:42 +0700

    Add function2 in Feature1

--- src/main/java/Feature1.java
+++ src/main/java/Feature1.java
@@ -5,4 +5,8 @@ public class Feature1 {
         System.out.println("function1");
         System.out.println("change1");
     }
+    public void function2()
+    {
+        System.out.println("function2");
+    }
 }
