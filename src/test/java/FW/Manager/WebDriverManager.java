6e46e4a9f2936e281df6a0ef472d5992a65bb57a
thanh.nguyen <thanhvirtium@gmail.com>
2020-10-27 22:15:41 +0700

    Build framework

--- src/test/java/FW/Manager/WebDriverManager.java
+++ src/test/java/FW/Manager/WebDriverManager.java
@@ -20,7 +20,7 @@ public class WebDriverManager {
     {
         driverType = DriverType.CHROME;
         environmentType = EnvironmentType.LOCAL;
-        System.out.println("222222222222222");
+        System.out.println("111111111111111");
     }
 
     public WebDriver getDriver()
